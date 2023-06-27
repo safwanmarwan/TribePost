2f27fce13c1fe3116d06c7ccdb028c2a19f80b03
Safwan Marwan <safwan.marwan@tmrnd.com.my>
2023-06-28 06:41:30 +0800

    Feat: Initial commit + Add PostList component + Update main component + Add PrimeNG + Add Firebase hosting config

---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  posts: any[] = [];
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
+
+  getPosts(): void {
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(posts => {
+        this.posts = posts;
+      });
+  }
+}
